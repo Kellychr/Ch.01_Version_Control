446c13d85a53f81145db2872b226da5bec2e72e1
kellychr <kellychr@urbandaleschools.com>
2022-09-09 08:59:30 -0500

    python Practice

--- 1.1_turtorial.py
+++ 1.1_turtorial.py
@@ -8,28 +8,64 @@ Turtle Documentation: https://docs.python.org/3.3/library/turtle.html?highlight=
 
 '''
 import turtle
-yoda=turtle.Turtle()
-screen=turtle.Screen() # makes a screen object
-screen.bgcolor('black') # colors the screen
-yoda.pensize(3) # width of pen line
-yoda.speed(10)  # speed of drawing. Go fast to not waste time.
-yoda.color("#00FF00")
-yoda.circle(100)  #head
-yoda.penup()
-yoda.setpos(50,185) #right ear
-yoda.pendown()
-yoda.goto(200,210)
-yoda.goto(88,145)
-yoda.penup()
-yoda.setpos(-50,185)  #left ear
-yoda.pendown()
-yoda.goto(-200,210)
-yoda.goto(-88,145)
-yoda.penup()
-yoda.setpos(200,-300)
-yoda.pendown()
-yoda.pencolor('#00FF00')
+
+g = turtle.Turtle()
+g.shape('turtle')
+
+g.color("green")
+g.forward(150)
+g.backward(300)
+g.goto(0,0)
+g.goto(37.5,0)
+g.begin_fill()
+g.color("red")
+g.left(90)
+g.forward(50)
+g.left(90)
+g.forward(75)
+g.left(90)
+g.forward(50)
+g.end_fill()
+g.penup()
+g.color("black")
+g.goto(-37.5,50)
+g.pendown()
+g.begin_fill()
+g.right(135)
+g.goto(0,70)
+g.goto(37.5,50)
+g.end_fill()
+g.penup()
+g.goto(150,0)
+g.pendown()
+g.color("green")
+g.begin_fill()
+g.right(60)
+g.circle(50,210)
+g.end_fill()
+g.penup()
+g.goto(-150,0)
+g.color("grey")
+g.right(210)
+g.pendown()
+g.begin_fill()
+g.forward(50)
+g.right(140)
+g.forward(50)
+g.left(150)
+g.forward(70)
+g.right(150)
+g.forward(80)
+g.end_fill()
+g.penup()
+g.goto(-50,140)
+g.pendown()
+g.begin_fill()
+g.color("yellow")
+g.circle(40,360)
+g.end_fill()
+g.goto(50,140)
 
 
-yoda.write('Sign Your Name Here',font=("Arial", 16, "normal")) # signs your name to your art
+g.write('Christian Kelly',font=("Arial", 16, "normal")) # signs your name to your art
 turtle.exitonclick() #Keeps pycharm window open so we can see the drawing
